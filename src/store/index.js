dce3a66df0d00608c4bc7c78988ea94225ddf7df
syoungstone <syoungstone@gmail.com>
2020-10-04 16:13:01 -0400

    Fixed issues

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import firebase from '../firebase'
+import fb from '../firebase'
 import router from '../router/index'
 
 Vue.use(Vuex)
@@ -9,24 +9,31 @@ export default new Vuex.Store({
   state: {
     userProfile: {}
   },
-  mutations: {},
+  mutations: {
+    setUserProfile(state, val) {
+      state.userProfile = val
+    }
+  },
   actions: {
     async login({ dispatch }, form) {
       // sign user in
-      const { user } = await fb.auth.signInWithEmailAndPassword(form.email, form.password)
-  
+      const { user } = await fb.auth.signInWithEmailAndPassword(
+        form.email,
+        form.password
+      )
+
       // fetch user profile and set in state
       dispatch('fetchUserProfile', user)
     },
     async fetchUserProfile({ commit }, user) {
       // fetch user profile
       const userProfile = await fb.usersCollection.doc(user.uid).get()
-  
+
       // set user profile in state
       commit('setUserProfile', userProfile.data())
-      
+
       // change route to dashboard
       router.push('/')
-  },
-  modules: {}
+    }
+  }
 })
